0c0b7477db8ad36721b8d0b8fa3fb768385b8229
bbangmaru <hobin1217@soongsil.ac.kr>
2022-07-11 22:27:53 +0900

    1.1 Authentication Frame Complete

---
+++ src/components/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ComponentRouter from "./Router";
+
+function App() {
+  return <ComponentRouter />
+}
+
+export default App;
---
+++ src/components/Router.js
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import Auth from "../routes/Auth";
+import Home from "../routes/Home";
+
+const ComponentRouter = () => {
+    const [isLoggedIn, setIsLoggedIn] = useState(true);
+    return (
+        <Router>
+            <Routes>
+                { isLoggedIn ?
+                <>
+                <Route exact path="/" element={<Home />} />
+                </> : <Route exact path="/" element={<Auth />} />}
+            </Routes>
+        </Router>
+    );
+};
+
+export default ComponentRouter;
